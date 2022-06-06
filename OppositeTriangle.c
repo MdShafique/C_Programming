7a0e4bb5c71d3d4ff30a0f5fd5aced7b05841ac4
Absilon Shafique <somono65@gmail.com>
2022-06-05 22:53:44 -0700

    update

---
+++ OppositeTriangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+    int i,j,n;
+    printf("Enter the N: ");
+    scanf("%d", &n);
+    for(i=1; i<=n; i++){
+        printf("\n");
+        for(j=1; j<=n-i; j++)
+            printf("*");
+    }
+}
